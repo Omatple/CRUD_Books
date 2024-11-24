71ae4e09a22f123f7b2f30b127d53f6f798633b6
omatple <omatple@gmail.com>
2024-11-24 13:00:59 +0100

    Empiezo CRUD Books

---
+++ database/tables.sql
@@ -0,0 +1,18 @@
+-- Table authors
+CREATE TABLE authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    country VARCHAR(80) NOT NULL
+);
+
+-- Table books
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) UNIQUE NOT NULL,
+    synopsis TEXT NOT NULL,
+    cover VARCHAR(150) NOT NULL,
+    author_id INT,
+    CONSTRAINT fk_book_author FOREIGN KEY (author_id) REFERENCES authors(id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);

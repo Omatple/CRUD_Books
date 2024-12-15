6d74413ca04c74846b46670824bac6c4207eda6e
omatple <omatple@gmail.com>
2024-12-15 17:52:28 +0100

    fix: Resuelto conflicto en generateFakeBooks.php

--- scripts/generateFakeBooks.php
+++ scripts/generateFakeBooks.php
@@ -1,28 +1,11 @@
 <?php
 
-<<<<<<< HEAD
-use App\Database\Author;
-use App\Database\Book;
-=======
 use App\Database\AuthorRepository;
 use App\Database\BookRepository;
->>>>>>> bc9b800 (feat: Finalización y refactorización completa del proyecto CRUD_Books con documentación detallada)
 
 require __DIR__ . "/../vendor/autoload.php";
 
 do {
-<<<<<<< HEAD
-    $amount = (int) readline("Write number of books you want create(5-50), or 0 to exit: ");
-    if ($amount === 0) exit("\nExit for request of user..." . PHP_EOL);
-    if ($amount < 5 || $amount > 50) echo "\nERROR: Please write a number between 5 and 50." . PHP_EOL;
-} while ($amount < 5 || $amount > 50);
-
-Book::restoreBooks();
-Author::restoreAuthors();
-Author::generateFakeAuthors();
-Book::generateFakeBooks($amount);
-echo "\n$amount fake books has been created successly." . PHP_EOL;
-=======
     $amount = (int) readline("Write the number of books you want to create (5-50), or 0 to exit: ");
     if ($amount === 0) exit("\nExiting at user request..." . PHP_EOL);
     if ($amount < 5 || $amount > 50) echo "\nERROR: Please enter a number between 5 and 50." . PHP_EOL;
@@ -34,4 +17,3 @@ AuthorRepository::generateFakeAuthors();
 BookRepository::generateFakeBooks($amount);
 
 echo "\n$amount fake books have been successfully created." . PHP_EOL;
->>>>>>> bc9b800 (feat: Finalización y refactorización completa del proyecto CRUD_Books con documentación detallada)
